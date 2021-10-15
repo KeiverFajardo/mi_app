5c1ffba38bd0da1353759ae9688a423dcb63116a
Keiver Fajardo <ing.kfajardo@gmail.com>
2021-10-14 23:57:21 -0300

    firebase3

--- src/Componentes/Cart/Cart.jsx
+++ src/Componentes/Cart/Cart.jsx
@@ -1,12 +1,102 @@
+
+import { useState } from 'react';
 import React from 'react';
 import { useCartContext } from '../../context/cartContext';
 import { Link } from 'react-router-dom'
+import { getFirestore } from '../../services/getFirebase';
+import firebase from 'firebase';
+import 'firebase/firestore'
 
 
 const Cart = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        tel: '',
+        email: ''
+    })
+
+    const {carList, deleteItemCart, precioTotal} = useCartContext()
     
-    const {carList, deleteItemCart} = useCartContext()
-    console.log(carList)
+  
+    const handleOnSubmit = (e) =>{
+        e.preventDefault()
+        let orden = {}
+        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
+
+        orden.buyer = formData
+
+        orden.total = precioTotal();
+
+        orden.items = carList.map(cartItem => {
+            const id = cartItem.item.id;
+            const title = cartItem.item.nombre;
+            const price = cartItem.item.precio * cartItem.cant;
+
+            return {id, title, price}
+        })
+
+
+
+
+        const db = getFirestore();
+        const ordersCol = db.collection('orders');
+        //db.collection('orders').doc(id).set(orden);
+
+        //
+        ordersCol.add(orden)
+        .then((IdDocumento) => {
+            console.log(IdDocumento.id)
+        })
+        .catch( err => {
+            console.log(err);
+        })
+        .finally(() => {
+            setFormData({
+                name: '',
+                tel: '',
+                email: ''
+
+            })
+          //  borrarLista()
+            console.log('termino la promesa')
+        })
+
+      //  db.collection('orders').doc('6xPoNdVli5Vz2c6QF2OE').update({total: 600}).then(resp => console.log(resp)) ejemplo de actualizar
+
+      //actualiza todos los items que estan en el listado del cart del cartcontext
+        const itemsToUpdate = db.collection('items').where(
+            firebase.firestore.FieldPath.documentId(), 'in', carList.map(i => i.item.id)
+        )
+
+        const batch = db.batch();
+
+        itemsToUpdate.get()
+        .then( collection=> {
+            collection.docs.forEach(docSnapshot => {
+                batch.update(docSnapshot.ref, {
+                    stock: docSnapshot.data().stock - carList.find(item => item.item.id === docSnapshot.id).cant
+                })
+            })
+
+            batch.commit().then(res => {
+                console.log('resultado batch', res)
+            })
+        })
+        console.log(orden)
+    }
+    
+
+    function handleOnChange(e) {
+        
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+        
+        
+        
+    }
+    console.log(formData)
 
     return (
         <div>
@@ -21,32 +111,71 @@ const Cart = () => {
                     </div>
                     
                 :
-                    carList.map(item => 
-                                            <div className="card product-card" key={item.id}>
-                                                <div className="card w-90 mt-6">
-                                                    <div className="col-6">
-                                                        <img src={item.item.foto} alt="" className='w-30'/>
-                                                    </div>
-                                                    <div className="row">
-                                                        <h3>{item.item.nombre}</h3>
-                                                        <span>Categoria: {item.item.categoria}</span> 
-                                                        <p>Descripcion: {item.item.descripcion}</p> 
-                                                        <p>Precio: ${item.item.precio}</p> 
-                                                        <p>Cantidad: {item.cant}</p> 
-                                                    </div>
-                    
-                                                </div>    
-                                      
-                                                <button className="btn btn-outline-danger w-250" onClick={() => deleteItemCart(item)}>Eliminar</button>
-                                                <hr />
-                                            </div> 
-                                )
-                }
-                  
+                    <div>
+
+                        
+                        {carList.map(item => 
+                                                <div className="card product-card" key={item.id}>
+                                                    <div className="card w-90 mt-6">
+                                                        <div className="col-6">
+                                                            <img src={item.item.foto} alt="" className='w-30'/>
+                                                        </div>
+                                                        <div className="row">
+                                                            <label>{item.item.nombre}</label>
+                                                            <span>Categoria: {item.item.categoria}</span> 
+                                                            <label>Descripcion: {item.item.descripcion}</label> 
+                                                            <label>Precio: $ {item.item.precio}</label> 
+                                                            <label>Cantidad: {item.cant}</label> 
+                                                        </div>
+                        
+                                                    </div>    
+                                        
+                                                    <button className="btn btn-outline-danger w-250" onClick={() => deleteItemCart(item)}>Eliminar</button>
+                                                    <hr />
+                                                </div> 
+                        )}
                 
-            </center>     
+
+                                    <form 
+                                        onSubmit={handleOnSubmit}
+                                        onChange={handleOnChange}
+                                    >
+                                        <div className="row">
+                                            <input className="btn btn-outline-info w-50 mt-2 mr-2"
+                                                type="text"
+                                                placeholder='Ingrese el nombre'
+                                                name='name' 
+                                                value={formData.name}
+                                            />
+                                            <input className="btn btn-outline-info w-50 mt-2 mr-2"
+                                                type="text"
+                                                placeholder='Ingrese el numero de telefono'
+                                                name='tel'
+                                                value={formData.tel} 
+                                            />
+                                            <input className="btn btn-outline-info w-50 mt-2 mr-2"
+                                                type="text"
+                                                placeholder='Ingrese el email'
+                                                name='email'
+                                                value={formData.email} 
+                                            />
+                                            <input className="btn btn-outline-info w-50 mt-2 mr-2"
+                                                type="text"
+                                                placeholder='Confirme el email'
+                                                name='email2' 
+                                                
+                                            />
+                                            <button className="btn btn-outline-success w-100 mt-2">Terminar Compra</button>
+                                        </div>
+                                    </form>
+
+
+                    </div>
+                }
+            </center>
+
         </div>
-    );
+    )
 
 }
 
